793c473226552baab6ae06415863d6c06ec2cb11
Iván Ruiz <ivan.ruiz@uca.es>
2022-10-27 17:31:21 +0200

    Carga inicial del proyecto base

---
+++ miappvaadin/src/main/java/com/example/application/views/MainLayout.java
@@ -0,0 +1,110 @@
+package com.example.application.views;
+
+
+import com.example.application.views.about.AboutView;
+import com.example.application.views.creditcardform.CreditCardFormView;
+import com.example.application.views.helloworld.HelloWorldView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.router.RouterLink;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+public class MainLayout extends AppLayout {
+
+    /**
+     * A simple navigation item component, based on ListItem element.
+     */
+    public static class MenuItemInfo extends ListItem {
+
+        private final Class<? extends Component> view;
+
+        public MenuItemInfo(String menuTitle, String iconClass, Class<? extends Component> view) {
+            this.view = view;
+            RouterLink link = new RouterLink();
+            // Use Lumo classnames for various styling
+            link.addClassNames("flex", "gap-xs", "h-m", "items-center", "px-s", "text-body");
+            link.setRoute(view);
+
+            Span text = new Span(menuTitle);
+            // Use Lumo classnames for various styling
+            text.addClassNames("font-medium", "text-m", "whitespace-nowrap");
+
+            link.add(new LineAwesomeIcon(iconClass), text);
+            add(link);
+        }
+
+        public Class<?> getView() {
+            return view;
+        }
+
+        /**
+         * Simple wrapper to create icons using LineAwesome iconset. See
+         * https://icons8.com/line-awesome
+         */
+        @NpmPackage(value = "line-awesome", version = "1.3.0")
+        public static class LineAwesomeIcon extends Span {
+            public LineAwesomeIcon(String lineawesomeClassnames) {
+                // Use Lumo classnames for suitable font styling
+                addClassNames("text-l", "text-secondary");
+                if (!lineawesomeClassnames.isEmpty()) {
+                    addClassNames(lineawesomeClassnames);
+                }
+            }
+        }
+
+    }
+
+    public MainLayout() {
+        addToNavbar(createHeaderContent());
+    }
+
+    private Component createHeaderContent() {
+        Header header = new Header();
+        header.addClassNames("box-border", "flex", "flex-col", "w-full");
+
+        Div layout = new Div();
+        layout.addClassNames("flex", "items-center", "px-l");
+
+        H1 appName = new H1("MiAppVaadin");
+        appName.addClassNames("my-m", "me-auto", "text-l");
+        layout.add(appName);
+
+        Nav nav = new Nav();
+        nav.addClassNames("flex", "overflow-auto", "px-m", "py-xs");
+
+        // Wrap the links in a list; improves accessibility
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("flex", "gap-s", "list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (MenuItemInfo menuItem : createMenuItems()) {
+            list.add(menuItem);
+
+        }
+
+        header.add(layout, nav);
+        return header;
+    }
+
+    private MenuItemInfo[] createMenuItems() {
+        return new MenuItemInfo[]{ //
+                new MenuItemInfo("Hello World", "la la-globe", HelloWorldView.class), //
+
+                new MenuItemInfo("About", "la la-file", AboutView.class), //
+
+                new MenuItemInfo("Credit Card Form", "la la-credit-card", CreditCardFormView.class), //
+
+        };
+    }
+
+}
